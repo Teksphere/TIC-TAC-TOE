192b6be164a2a7bb47702e7b2a7104d6a316d7c7
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-10-28 11:05:52 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "teksphere-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "observalability-242" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
